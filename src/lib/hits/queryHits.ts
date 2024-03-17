30322ca7e104c10bcdff5cf9b012543d3da25b6a
arnorhs <arnorhs@gmail.com>
2024-03-17 16:54:08 +0000

    rockin it

--- src/lib/hits/queryHits.ts
+++ src/lib/hits/queryHits.ts
@@ -3,56 +3,45 @@ import { db, Hits, sql, eq, and, gt, gte } from 'astro:db'
 export type Period = 'hour' | 'day' | 'week' | 'month' | 'year'
 
 function getFormat(period: Period, today: Date) {
+  let dateMask
   let format
-  let fromDate
   let interval
   switch (period) {
     case 'hour':
       format = 'YYYY-MM-DDTHH-mm'
       interval = 60 * 1000
-      fromDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate(),
-        today.getHours() - 1,
-        today.getMinutes(),
-      )
+      dateMask = [0, 0, 0, -1, 0]
       break
     case 'day':
       format = 'YYYY-MM-DDTHH'
       interval = 60 * 60 * 1000
-      fromDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate() - 1,
-        today.getHours(),
-      )
+      dateMask = [0, 0, -1, 0, 0]
       break
     case 'week':
     case 'month':
       format = 'YYYY-MM-DD'
       interval = 60 * 60 * 24 * 1000
-      fromDate = new Date(
-        today.getFullYear(),
-        today.getMonth() - 1,
-        today.getDate(),
-        today.getHours(),
-      )
+      dateMask = [0, -1, 0, 0, 0]
       break
     case 'year':
       format = 'YYYY-MM-DD'
       // TODO: use week intervals?
       interval = 60 * 60 * 24 * 1000
-      fromDate = new Date(
-        today.getFullYear() - 1,
-        today.getMonth(),
-        today.getDate(),
-        today.getHours(),
-      )
+      dateMask = [-1, 0, 0, 0, 0]
       break
   }
 
-  return { format, fromDate, interval }
+  return {
+    format,
+    fromDate: new Date(
+      today.getFullYear() + dateMask[0],
+      today.getMonth() + dateMask[1],
+      today.getDate() + dateMask[2],
+      today.getHours() + dateMask[3],
+      today.getMinutes() + dateMask[4],
+    ),
+    interval,
+  }
 }
 
 export async function queryHits(path: string, period: Period, today: Date) {
@@ -76,17 +65,15 @@ export async function queryHits(path: string, period: Period, today: Date) {
     {} as { [key: string]: number },
   )
 
-  let hits = [] as { thedate: string; hits: number }[]
+  let hits = [] as { thedate: Date; hits: number }[]
 
   // the loop ends up being inclusive of today, since the time comparison is less fine grained
   // than what we are iterating by - we are ok with for more understandable code
-  for (let i = new Date(fromDate); i < today; i.setTime(i.getTime() + interval)) {
+  for (let i = new Date(fromDate); i < today; i = new Date(i.getTime() + interval)) {
     const key = i.toISOString().substring(0, format.length)
 
-    hits.push({ thedate: key, hits: mapped[key] ?? 0 })
+    hits.push({ thedate: i, hits: mapped[key] ?? 0 })
   }
 
-  const max = Math.max(...hits.map((hit) => hit.hits), 0)
-
-  return { hits, max }
+  return hits
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#F7F7F8',
+          100: '#EBE8F2',
+          200: '#CFC4E9',
+          300: '#AE94E5',
+          400: '#8D63E9',
+          500: '#6821FC',
+          600: '#571ED2',
+          700: '#4B269C',
+          800: '#402A6F',
+          900: '#302744',
+          950: '#28262C',
+        },
+      },
+    },
+  },
+  plugins: [],
 }
